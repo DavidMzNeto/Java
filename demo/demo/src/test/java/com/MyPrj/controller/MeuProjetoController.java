b87c0c9358524fb1c26a58142ff48d4e34b1a68e
David Mizrahy Neto <160736321+DavidMzNeto@users.noreply.github.com>
2025-02-27 19:26:10 -0300

    Add files via upload

---
+++ demo/demo/src/test/java/com/MyPrj/controller/MeuProjetoController.java
@@ -0,0 +1,12 @@
+package com.MyPrj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeuProjetoController {
+    @GetMapping("/test")
+    public String helloWorld(){
+        return "Hello,World";
+    } 
+}
